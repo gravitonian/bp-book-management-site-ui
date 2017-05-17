018432167f8c3f31c031530bab3e0666b3a2567f
Martin Bergljung <gravitonian@gmail.com>
2017-05-17 16:28:39 +0100

    Book Events Dashlet, Book Status Page

---
+++ src/main/resources/META-INF/resources/book-management-site-ui/js/bestpub/widgets/BookStatusWidget.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2015 Alfresco Software Limited.
+ *
+ * This file is part of the Alfresco SDK Samples project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Aikau Widget displaying book status
+ *
+ * @module bestpub/widgets/BookStatusWidget
+ * @extends dijit/_WidgetBase
+ * @author martin.bergljung@marversolutions.org
+ */
+ define(["dojo/_base/declare",
+        "dijit/_WidgetBase",
+        "alfresco/core/Core",
+        "dijit/_TemplatedMixin",
+        "dojo/text!./templates/BookStatusWidget.html"
+    ],
+    function(declare, _Widget, Core, _Templated, template) {
+        return declare([_Widget, Core, _Templated], {
+            templateString: template,
+            i18nRequirements: [ {i18nFile: "./i18n/BookStatusWidget.properties"} ],
+            cssRequirements: [{cssFile:"./css/BookStatusWidget.css"}],
+            
+            buildRendering: function example_widgets_TemplateWidget__buildRendering() {
+                this.greeting = this.message('hello-label');
+
+                this.inherited(arguments);
+
+            }
+        });
+});
---
+++ src/main/resources/alfresco/web-extension/site-webscripts/org/marversolutions/bestpublishing/pages/book-status-page.get.js
@@ -0,0 +1,29 @@
+model.jsonModel = {
+    widgets: [{
+        id: "SET_PAGE_TITLE",
+        name: "alfresco/header/SetTitle",
+        config: {
+            title: "Book Status"
+        }
+    },
+        {
+            id: "BOOKSTATUS_PAGE_HORIZONTAL_WIDGET_LAYOUT",
+            name: "alfresco/layout/HorizontalWidgets",
+            config: {
+                widgetWidth: 50,
+                widgets: [
+                    {
+                        id: "BOOKSTATUS_PAGE_LOGO",
+                        name: "alfresco/logo/Logo",
+                        config: {
+                            logoClasses: "alfresco-logo-only"
+                        }
+                    },
+                    {
+                        id: "BOOKSTATUS_PAGE_WIDGET",
+                        name: "bestpubwidgets/BookStatusWidget"
+                    }
+                ]
+            }
+        }]
+};
